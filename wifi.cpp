aab41534221354bae09cb51d554ddb7995213ec8
Daniel Campello <campello@google.com>
2019-06-05 19:53:18 -0600

    Add Google DNS server when using static IP config

--- wifi.cpp
+++ wifi.cpp
@@ -14,7 +14,8 @@ void WiFiConfig::Setup() {
     mode_ = WIFI_STA;
     WiFi.mode(mode_);
     if (static_ip_) {
-      WiFi.config(ip_, gateway_, subnet_);
+      IPAddress dns(8,8,8,8); // Google Public DNS
+      WiFi.config(ip_, gateway_, subnet_, dns);
     }
     WiFi.begin(ssid_, pass_);
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
